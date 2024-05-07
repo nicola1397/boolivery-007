aebbe25ba05139e4c604836c599ec65c75eab017
GrPaladino <paladino.graziano@gmail.com>
2024-05-07 11:40:28 +0200

    Feat: test APi call

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,5 +1,6 @@
 <script>
-// import { store } from "../store";
+import { store } from "../store";
+import axios from "axios";
 
 import AppCard from "./AppCard.vue";
 
@@ -7,19 +8,33 @@ export default {
   data() {
     return {
       title: "Restaurants",
+      store,
+      restaurants: [],
     };
   },
 
   components: { AppCard },
 
-  methods: {},
+  methods: {
+    fetchRestaurants() {
+      axios.get(store.baseApiURI + "restaurants").then((response) => {
+        this.restaurants = response.data.restaurants;
+      });
+    },
+  },
+
+  created() {
+    this.fetchRestaurants();
+  },
 };
 </script>
 
 <template>
   <h1>{{ title }}</h1>
 
-  <app-card />
+  <p v-for="restaurant in restaurants">
+    {{ restaurant.name }}
+  </p>
 </template>
 
 <style lang="scss" scoped>
