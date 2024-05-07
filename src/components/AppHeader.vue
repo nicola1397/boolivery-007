63dcd8a7a2dba3205a4cb781256486461aad966e
nicola1397 <nicola1397@outlook.it>
2024-05-07 18:46:53 +0200

    feat: added logo to header and assets folder

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,16 +7,16 @@ export default {
     };
   },
 
-  methods: {
-},
-
-}
+  methods: {},
+};
 </script>
 
 <template>
-   <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">DeliveBoo</a>
+  <nav class="headerNav navbar navbar-expand-lg">
+    <div class="container-md">
+      <a class="navbar-brand" href="#"
+        ><img src="../assets/img/boolivery.svg" alt=""
+      /></a>
       <button
         class="navbar-toggler"
         type="button"
@@ -24,19 +24,28 @@ export default {
         data-bs-target="#navbarNav"
         aria-controls="navbarNav"
         aria-expanded="false"
-        aria-label="Toggle navigation">
+        aria-label="Toggle navigation"
+      >
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
-
           <li class="nav-item">
-            <router-link :to="{ name: 'home' }" class="nav-link" aria-current="page">Home</router-link>
+            <router-link
+              :to="{ name: 'home' }"
+              class="nav-link text-white"
+              aria-current="page"
+              >Home</router-link
+            >
           </li>
           <li class="nav-item">
-            <router-link :to="{ name: 'restaurants.index' }" class="nav-link" href="#">Restaurants</router-link>
+            <router-link
+              :to="{ name: 'restaurants.index' }"
+              class="nav-link text-white"
+              href="#"
+              >Restaurants</router-link
+            >
           </li>
-          
         </ul>
       </div>
     </div>
@@ -47,4 +56,18 @@ export default {
 @use "../style/partials/mixins" as *;
 @use "../style/partials/variables" as *;
 
+.headerNav {
+  height: 80px;
+  box-shadow: 0 0 5px black;
+  background: #2929b9;
+
+  img {
+    height: 50px;
+  }
+
+  a:hover {
+    transform: scale(1.1);
+    transition: all 0.1s ease 0.1s;
+  }
+}
 </style>
